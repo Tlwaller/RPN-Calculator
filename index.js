fb0138f43c98b4cc230708f33a7991110aa08287
tlwaller <tlwaller99@gmail.com>
2022-04-14 23:23:43 -0500

    added user input with ability to end process

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 //i've just discovered these chalk packages and am having way too much fun with them
 import chalk from "chalk";
 import chalkAnimation from "chalk-animation";
+import inquirer from "inquirer";
+
+let userInput;
 
 //just a little timer that can run before running other tasks
 const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
@@ -24,7 +27,29 @@ const welcome = async () => {
     ${chalk.yellowBright(-13)}
     > 13 +
     ${chalk.yellowBright(0)}
+    
+    ${chalk.bgRedBright("enter 'q' to quit")}
   `);
 };
 
+const getInput = async () => {
+  //grab input from user and store it in userInput
+  const answers = await inquirer.prompt({
+    name: "equation",
+    type: "input",
+  });
+  userInput = answers.equation;
+
+  //exit process if user sent "q"
+  if (userInput.includes("q")) {
+    chalkAnimation.pulse("Closing calculator, goodbye!");
+    await sleep();
+    await process.exit(1);
+  } else {
+    console.log("you entered: " + userInput);
+    await process.exit(1);
+  }
+};
+
 await welcome();
+await getInput();
